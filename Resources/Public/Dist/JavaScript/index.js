08128553eca6b752d1128a68bfd1c572ee5da2e4
Thilo Ratnaweera <thilo.ratnaweera@netbrothers.de>
2026-05-11 14:27:45 +0200

    ✨ Use color variables to provide better customisability.

--- Resources/Public/Dist/JavaScript/index.js
+++ Resources/Public/Dist/JavaScript/index.js
@@ -1,3 +1,3 @@
 //#region Resources/Private/JavaScript/index.js
-console.log("You are using NetBrothers TYPO3 Base Template. Thanks! ❤️");
+console.log("You are using the NetBrothers TYPO3 Base Template. Thanks! ❤️");
 //#endregion
